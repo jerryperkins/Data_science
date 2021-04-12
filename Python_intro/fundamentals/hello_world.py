c6fd95e0be337d3734dbe9b17f0c1d2397c90324
Jerry Perkins <67806599+jerryperkins@users.noreply.github.com>
2021-04-11 22:00:51 -0500

    Add files via upload

---
+++ Python_intro/fundamentals/hello_world.py
@@ -0,0 +1,12 @@
+print("hello world")
+
+name = "noelle"
+print("hello", name, "!")
+print("hello " + name + "!")
+number = 42
+print("hello", 42)
+# print("hello" + 42)
+fav_food1 = "sushi"
+fav_food2 = "pizza"
+print("I love to eat {} and {}" .format(fav_food1, fav_food2))
+print(f"I love to eat {fav_food1} and {fav_food2}")
